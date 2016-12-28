cfa9cfe5ce8b67433e08dd0da8aa4aa619a7a5f9
Jakub_Jirous <jakub.jirous@tul.cz>
2016-12-28 16:10:31 +0100

    Hot-reloading setup

--- server/index.js
+++ server/index.js
@@ -3,14 +3,24 @@ import path from 'path';
 
 import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpackConfig from '../webpack.config.dev';
 
 
 let app = express();
 
-// webpack middleware to creating JS bundle
-app.use(webpackMiddleware(webpack(webpackConfig)));
+// webpack config
+const compiler = webpack(webpackConfig);
 
+// webpack middleware to creating JS bundle
+app.use(webpackMiddleware(compiler));
+
+// webpack hot reload
+app.use(webpackHotMiddleware(compiler, {
+    hot: true,
+    publicPath: webpackConfig.output.publicPath,
+    noInfo: true
+}));
 
 // serving index.html from server to client
 app.get('/*', (req, res) => {
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,17 +1,28 @@
-import path from 'path'
+import path from 'path';
+import webpack from 'webpack';
 
 export default {
     devtools: 'eval-source-map',
-    entry: path.join(__dirname, '/client/index.js'),
+    entry: [
+        'webpack-hot-middleware/client',
+        path.join(__dirname, '/client/index.js'),
+    ],
     output: {
-        path: '/'
+        path: '/',
+        publicPath: '/'
     },
+    plugins: [
+        // configuring webpack middleware
+        new webpack.NoErrorsPlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
     module: {
         loaders: [
             {
                 test: /\.js$/,
                 include: path.join(__dirname, 'client'),
-                loaders: [ 'babel' ]
+                loaders: [ 'react-hot', 'babel' ]
             }
         ]
     },
